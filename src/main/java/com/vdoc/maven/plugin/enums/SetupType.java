46eebea38c6269f1e7405f51125c4c7dbf4cc190
famaridon <rolfone@rolfone-VirtualBox.(none)>
2014-10-23 21:15:00 +0200

    clean

--- src/main/java/com/vdoc/maven/plugin/enums/SetupType.java
+++
@@ -1,9 +0,0 @@
-package com.vdoc.maven.plugin.enums;
-
-/**
- * Created by famaridon on 13/05/2014.
- */
-public enum SetupType
-{
-	VDocApps, VDocCustom;
-}
--- src/main/java/com/vdoc/maven/plugin/utils/GobblerAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.vdoc.maven.plugin.utils;
-
-/**
- * Created by famaridon on 06/07/2014.
- */
-public interface GobblerAdapter
-{
-	public void println(String message);
-}
